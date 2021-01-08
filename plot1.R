a2f0d6764dab82a3630936444728fda85d643463
Kovani09 <76171969+Kovani09@users.noreply.github.com>
2021-01-09 00:01:21 +0200

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -10,5 +10,5 @@ data1$Date <- as.Date(data1$Date, format = "%d/%m/%Y")
 # Generating Plot 1
 hist(data1$Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency", col = "red")
 
-png("plot1.png", width = 480, height = 480)
+dev.copy(png, file = "plot1.png", width = 480, height = 480)
 dev.off()
